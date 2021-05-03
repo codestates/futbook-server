fee46b83b04d454c233638f148ba2a9c62e0d2c1
HyunJun Baek <hjbaek91@gmail.com>
2021-05-03 18:54:20 +0900

    feature_bookfutsal_#9: Added bookfutsal function for bookfutsal

--- controllers/booking/bookfutsal.js
+++ controllers/booking/bookfutsal.js
@@ -5,5 +5,18 @@ module.exports = (req, res) => {
   const { id, bookingDate } = req.body;
   const userSameId = await User.findOne({ where: id });
   const userSameDate = await User.findOne({ where: bookingDate })
+
+  if (userSameId) {
+    res.status(409).json({ message: 'You have already booked' })
+  } else if (userSameDate) {
+    res.status(409).json({ message: 'Cannot book twice on same day' })
+  } else {
+    try {
+      await User.create({ id, bookingDate });
+      res.status(200).send({ message: 'Booking successful' })
+    } catch (err) {
+      res.status(401).send({ message : 'Invaild token, failed to book' })
+    }
+  }
 };
 // res.send("test ahah, checkcheck");
