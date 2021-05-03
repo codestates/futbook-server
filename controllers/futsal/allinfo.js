02916aba0c2a22be1bc4af7f80f23806271b6bfc
민제현 <alswpgus0@gmail.com>
2021-05-03 18:55:28 +0900

    feature_futsalAllInfo_#28:Added allinfo function for allinfo.js

--- controllers/futsal/allinfo.js
+++ controllers/futsal/allinfo.js
@@ -1,3 +1,21 @@
 const { Futsal } = require("../../models");
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+  const result = await Futsal.findAll();
+
+  if (result.length === 0) {
+    res.status(400).send({ message: "no futsal data" });
+  } else {
+    const futsalDatas = result.map(futsal => {
+      futsal = futsal.dataValues;
+      delete futsal.createdAt;
+      delete futsal.user_Id;
+      return futsal;
+    });
+
+    res.status(200).send({
+      data: { futsalDatas },
+      message: "succeeded getting in futsalDatas",
+    });
+  }
+};
