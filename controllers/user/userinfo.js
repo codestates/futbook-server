95b0de3a62cc495498964e4f82a7f49687fa5e2e
Minwoo Ham <hammin330@knou.ac.kr>
2021-05-03 13:14:44 +0900

    feature_userInfo_#10:Added userinfo function for userinfo.js

--- controllers/user/userinfo.js
+++ controllers/user/userinfo.js
@@ -1,3 +1,15 @@
-const {} = require("../../models");
+const { User } = require("../../models");
 
-module.exports = (req, res) => {};
+module.exports = async(req, res) => {
+  const id = req.params.id
+
+  const userInfo = await User.findOne({ where: { id } });
+  
+  if(!userInfo){
+    res.status(400).send({ message: 'Invalid Id params' });
+  } else {
+    const { id, email, nickname } = userInfo; 
+    res.status(200).send({ data: { userInfo: { id, email, nickname }}, message: 'succeeded in getting userInfo'});
+  }
+};
+
