8f4b8b45fe4dabf86604d96f4814a10c44721a3d
민제현 <alswpgus0@gmail.com>
2021-05-03 10:54:43 +0900

    feature_signup_#16:Added signup function for signup.js

--- routes/sign.js
+++ routes/sign.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/sign");
 
-router.post("/siginin", controller.signin);
-router.post("/siginup", controller.signup);
+router.post("/signin", controller.signin);
+router.post("/signup", controller.signup);
 router.post("/signout", controller.signout);
 
 module.exports = router;
