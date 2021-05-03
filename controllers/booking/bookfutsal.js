0f320d298b1b9758c3303a380dcfff6a73b9b2b8
HyunJun Baek <hjbaek91@gmail.com>
2021-05-03 18:51:48 +0900

    feature_bookfutsal_#9: Fixed previous commit title

--- controllers/booking/bookfutsal.js
+++ controllers/booking/bookfutsal.js
@@ -4,7 +4,6 @@ module.exports = (req, res) => {
   // const { authorization } = req.headers;
   const { id, bookingDate } = req.body;
   const userSameId = await User.findOne({ where: id });
-  const userSameDate = await User.findOne({ where: {bookingDate} })
+  const userSameDate = await User.findOne({ where: bookingDate })
 };
-
 // res.send("test ahah, checkcheck");
