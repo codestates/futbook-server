78f7b6eef6ae7c0ce5f0b457e38d31ef5c888f8d
민제현 <alswpgus0@gmail.com>
2021-05-03 11:54:03 +0900

    feature_signout_#17:Added Id params for sign.js

--- routes/sign.js
+++ routes/sign.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/sign");
+const checkTokenValid = require("../middlewares/checkTokenValid");
 
 router.post("/signin", controller.signin);
 router.post("/signup", controller.signup);
-router.get("/signout", controller.signout);
+router.get("/signout/:id", checkTokenValid, controller.signout);
 
 module.exports = router;
