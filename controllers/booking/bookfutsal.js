302dc940aa8217518d31246d7088e6b38b34570d
민제현 <alswpgus0@gmail.com>
2021-05-05 19:24:54 +0900

    feature_Modifybookfutsalreq_#38:Modified bookfutsalres

--- controllers/booking/bookfutsal.js
+++ controllers/booking/bookfutsal.js
@@ -3,12 +3,15 @@ const { Booking } = require("../../models");
 module.exports = async (req, res) => {
   const user_Id = res.userId;
   const { futsal_Id, bookingDate } = req.body;
-
-  try {
-    const result = await Booking.create({ user_Id, futsal_Id, bookingDate });
-
-    res.status(200).send({ message: "booking successful" });
-  } catch (err) {
-    res.status(500).send({ message: `${err.message}` });
+  const result = await Booking.findOne({ where: { futsal_Id, bookingDate } });
+  if (!result) {
+    try {
+      await Booking.create({ user_Id, futsal_Id, bookingDate });
+      res.status(200).send({ message: "booking successful" });
+    } catch (err) {
+      res.status(500).send({ message: `${err.message}` });
+    }
+  } else {
+    res.status(409).send({ message: "already booked" });
   }
 };
